d4a2a6aaa109b328c453a90aeb8f25f2b4f0b683
Dennis <56777642+damoah2@users.noreply.github.com>
2019-10-29 23:09:13 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+name = input('What is your name?:')
+print('Hello',name)
+print('Welcome!!!')
+
+age = input('How old are you?:')
+print('Nice that means you will be')
+age = int(age)
+print(age +5)
+print('in five years cool!')
+
+school_name = input('What school do you go to?:')
+graduation_year = input ('When will you graduate?:')
+print ('Nice I hope you get your dream job!')
