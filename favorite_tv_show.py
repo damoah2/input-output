eede9b7b1584e71ada501a47b1265e34a016835d
Dennis <56777642+damoah2@users.noreply.github.com>
2019-11-30 09:36:55 -0500

    Add files via upload
    favorite tv show

---
+++ favorite_tv_show.py
@@ -0,0 +1,8 @@
+#ask question
+tv_show = input("What is your favorite tv show?")
+if tv_show == "The Flash" : #anwser to question
+    print("YEAH! Run Barry RUN!")
+else:
+    print("No try again")
+print("Thanks for playing")
+#finish code
